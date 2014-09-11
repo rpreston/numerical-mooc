cf50bc1d39169d9edd3132f6c07db0244d0db47b
rpreston <drummingmastr@gmail.com>
2014-09-10 22:56:33 -0500

    Update Notes

--- Notes/Paper Airplane.py
+++ Notes/Paper Airplane.py
@@ -15,8 +15,8 @@ rcParams['font.size'] = 16
 
 # model parameters:
 g = 9.8      # gravity in m s^{-2}
-v_t = 30.0   # trim velocity in m s^{-1}   
-C_D = 1/40.  # drag coefficient --- or D/L if C_L=1
+v_t = 4.9   # trim velocity in m s^{-1}   
+C_D = 1/5.  # drag coefficient --- or D/L if C_L=1
 C_L = 1.0    # for convenience, use C_L = 1
 
 ### set initial conditions ###
@@ -24,6 +24,8 @@ v0 = v_t     # start at the trim velocity (or add a delta)
 theta0 = 0.0 # initial angle of trajectory
 x0 = 0.0     # horizotal position is arbitrary
 y0 = 1000.0  # initial altitude
+
+
 
 def f(u):
     """Returns the right-hand side of the phugoid system of equations.
@@ -65,37 +67,9 @@ def euler_step(u, f, dt):
     u_n_plus_1 : array of float
         approximate solution at the next time step.
     """
-    
+    print u+dt*f(u)
     return u + dt * f(u)
     
-def get_diffgrid(u_current, u_fine, dt):
-    """Returns the difference between one grid and the fine one using L-1 norm.
-    
-    Parameters
-    ----------
-    u_current : array of float
-        solution on the current grid.
-    u_finest : array of float
-        solution on the fine grid.
-    dt : float
-        time-increment on the current grid.
-    
-    Returns
-    -------
-    diffgrid : float
-        difference computed in the L-1 norm.
-    """
-    
-    N_current = len(u_current[:,0])
-    N_fine = len(u_fine[:,0])
-   
-    grid_size_ratio = ceil(N_fine/float(N_current))
-    
-    diffgrid = dt * numpy.sum( numpy.abs(\
-            u_current[:,2]- u_fine[::grid_size_ratio,2])) 
-    
-    return diffgrid
-
 T = 100.0                          # final time
 dt = 0.1                           # time increment
 N = int(T/dt) + 1                  # number of time-steps
@@ -103,101 +77,64 @@ t = numpy.linspace(0.0, T, N)      # time discretization
 
 # initialize the array containing the solution for each time-step
 u = numpy.empty((N, 4))
-u[0] = numpy.array([v0, theta0, x0, y0])# fill 1st element with initial values
+# fill 1st element with initial values
 
-# time loop - Euler method
-for n in range(N-1):
+
+def nominalRun():
     
-    u[n+1] = euler_step(u[n], f, dt)
+    theta0 = 0.0
+    u[0] = numpy.array([v_t, theta0, x0, y0])
     
+    
+    for n in range(N-1):
+        u[n+1] = euler_step(u[n],f,dt)
+    x=u[:,2]
+    y=u[:,3]
+    plt.figure(figsize=(8,6))
+    plt.grid(True)
+    plt.xlabel(r'x', fontsize=18)
+    plt.ylabel(r'y', fontsize=18)
+    plt.title('Glider Nominal trajectory, flight time = %.2f' % T, fontsize=18)
+    plt.plot(x,y,"k-",lw=2)
+    plt.plot(t,x,'b-',lw=2)
+    plt.plot(t,y,'r',lw=2)
+
+def monteCarlo(iterations,maxArg,minArg):
+    xm = numpy.empty((iterations,N))
+    ym = numpy.empty((iterations,N))
+    
+    v0=v_t
+    theta0 = minArg
+    
+    
+    for i in range(0,iterations):
+        theta0 += (maxArg-minArg)/(iterations)
+        u = numpy.empty((N,4))
+        u[0] = numpy.array([v0, theta0, x0, y0])
+        
+        for n in range(N-1):
+            
+            u[n+1] = euler_step(u[n], f, dt)
+        
+        x = u[:,2]
+        y = u[:,3]
+        for p in range(N-1):
+            xm[i][p] = x[p]
+            ym[i][p] = y[p]
+    plt.figure(figsize=(8,6))
+    plt.grid(True)
+    plt.xlabel(r'x', fontsize=18)
+    plt.ylabel(r'y', fontsize=18)
+    plt.title('Glider trajectory, flight time = %.2f' % T, fontsize=18)
+    for temp in range(0,iterations):
+        plt.plot(xm[temp][:N-1],ym[temp][:N-1], 'k-',lw=1);
+        plt.plot(t[:N-1],xm[temp][:N-1],'b',lw=1)
+        plt.plot(t[:N-1],ym[temp][:N-1],'r',lw=1)
+    
+monteCarlo(50,numpy.pi,0.0)
+
+        
+
 # get the glider's position with respect to the time
-x = u[:,2]
-y = u[:,3]
-
+#monteCarlo(50,7,2)
 # visualization of the path
-plt.figure(figsize=(8,6))
-plt.grid(True)
-plt.xlabel(r'x', fontsize=18)
-plt.ylabel(r'y', fontsize=18)
-plt.title('Glider trajectory, flight time = %.2f' % T, fontsize=18)
-plt.plot(x,y, 'k-', lw=2);
-
-dt_values = numpy.array([0.1, 0.05, 0.01, 0.005, 0.001])
-
-u_values = numpy.empty_like(dt_values, dtype=numpy.ndarray)
-
-for i, dt in enumerate(dt_values):
-    
-    N = int(T/dt) + 1    # number of time-steps
-    
-    ### discretize the time t ###
-    t = numpy.linspace(0.0, T, N)
-    
-    # initialize the array containing the solution for each time-step
-    u = numpy.empty((N, 4))
-    u[0] = numpy.array([v0, theta0, x0, y0])
-
-    # time loop
-    for n in range(N-1):
-       
-        u[n+1] = euler_step(u[n], f, dt)   ### call euler_step() ###
-    
-    # store the value of u related to one grid
-    u_values[i] = u
-    
-# compute difference between one grid solution and the finest one
-diffgrid = numpy.empty_like(dt_values)
-
-for i, dt in enumerate(dt_values):
-    print('dt = {}'.format(dt))
-
-    ### call the function get_diffgrid() ###
-    diffgrid[i] = get_diffgrid(u_values[i], u_values[-1], dt)
-
-# log-log plot of the grid differences
-plt.figure(figsize=(6,6))
-plt.grid(True)
-plt.xlabel('$\Delta t$', fontsize=18)
-plt.ylabel('$L_1$-norm of the grid differences', fontsize=18)
-plt.axis('equal')
-plt.loglog(dt_values[:-1], diffgrid[:-1], color='k', ls='-', lw=2, marker='o');
-
-r = 2
-h = 0.001
-
-dt_values2 = numpy.array([h, r*h, r**2*h])
-
-u_values2 = numpy.empty_like(dt_values2, dtype=numpy.ndarray)
-
-diffgrid2 = numpy.empty(2)
-
-for i, dt in enumerate(dt_values2):
-    
-    N = int(T/dt)    # number of time-steps
-    
-    ### discretize the time t ###
-    t = numpy.linspace(0.0, T, N)
-    
-    # initialize the array containing the solution for each time-step
-    u = numpy.empty((N, 4))
-    u[0] = numpy.array([v0, theta0, x0, y0])
-
-    # time loop
-    for n in range(N-1):
-
-        u[n+1] = euler_step(u[n], f, dt)         ### call euler_step() ###
-    
-    # store the value of u related to one grid
-    u_values2[i] = u
-    
-
-#calculate f2 - f1
-diffgrid2[0] = get_diffgrid(u_values2[1], u_values2[0], dt_values2[1])
-
-#calculate f3 - f2
-diffgrid2[1] = get_diffgrid(u_values2[2], u_values2[1], dt_values2[2])
-
-# calculate the order of convergence
-p = (log(diffgrid2[1]) - log(diffgrid2[0])) / log(r)
-
-print('The order of convergence is p = {:.3f}'.format(p));
